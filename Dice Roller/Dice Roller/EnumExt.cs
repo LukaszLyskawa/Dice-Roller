9114c4e54e1f48d106f6656e9ab6d9c411782799
Lucas Ł <lukasz@lyskawa.pl>
2017-05-21 12:53:46 +0200

    added ext to enum

---
+++ Dice Roller/Dice Roller/EnumExt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_Roller
+{
+    public static class EnumExt
+    {
+
+        public static String ToString<T>(this T? value) where T : struct
+        {
+            return value.HasValue ? Enum.GetName(typeof(T), value) : null;
+        }
+
+        public static T? Parse<T>(this String value) where T : struct
+        {
+            return Enum.TryParse(value, out T v) ? (T?)v : null;
+        }
+
+
+    }
+}
